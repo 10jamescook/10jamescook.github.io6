1f831b71d64f1f68c80f16adc0f2dc46d16ea719
James <jamescook_10@hotmail.co.uk>
2026-02-05 21:12:46 +0000

    Added js code

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+function colourButton() {
+
+
 function red() { document.getElementById("header").style.color = "red";
 };
 
 function green() { document.getElementById("header").style.color = "green";
 };
+
+};
+
+export default colourButton;
